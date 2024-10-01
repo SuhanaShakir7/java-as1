fcccfc3caa660199125650233f671ae6f0a44ef9
SuhanaShakir7 <busheskhanbushes3271@gmail.com>
2024-10-01 23:36:38 +0500

    Add files via upload

---
+++ JAVA.HTML/script.js
@@ -0,0 +1,41 @@
+//  What does the alert function do in JavaScript?
+//  its shows some special message and sometimes warning or  specific  message.
+alert("Welcome to my website!");
+
+// What happens if you capitalize the alert keyword?
+// it will be not a good code  but  it  will be  identifer to  understand
+
+
+alert("Hello, world!");;
+
+// Write a JavaScript alert statement with proper formatting and explain each part of the
+// code.
+
+// alert this is a Syntax
+// ( )  this vbrackket  are use  in alert  
+// "hello world" in the string  show what we want to show in our alert.
+
+// chap 2
+ var message="Hello, world!";
+ alert(message);
+
+ var age=
+ age=25
+ alert(age)
+
+
+var  greeting="Good morning!"
+alert(greeting);
+
+
+var greeting="Good evening!"
+alert(greeting);
+
+
+var favoriteColor =" blue"
+alert(" My favorite color is" + favoriteColor );
+
+
+var  age=
+age=25
+alert(age);
